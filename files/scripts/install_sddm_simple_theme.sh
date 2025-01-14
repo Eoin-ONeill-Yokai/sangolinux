3ac5aa6b68d716d5aff6b7eb60047fd71bb6f1ca
Eoin O'Neill <eoinoneill1991@gmail.com>
2025-01-13 21:32:17 -0800

    Remove erroneous `sudo` use in setup script..

--- files/scripts/install_sddm_simple_theme.sh
+++ files/scripts/install_sddm_simple_theme.sh
@@ -7,7 +7,7 @@ echo "Installing Theme: simple-sddm ..."
 
 git clone https://github.com/JaKooLit/simple-sddm.git ./simple-sddm
 mkdir -p /usr/share/sddm/themes
-sudo mv ./simple-sddm /usr/share/sddm/themes/
+mv ./simple-sddm /usr/share/sddm/themes/
 
 cat <<EOF >> /etc/sddm.conf.d/sddm.conf
 [Theme]
