7abfb5cb2ec7c27a36b6ade8a0f8666d5a99a8cb
Eoin O'Neill <eoinoneill1991@gmail.com>
2025-01-13 20:42:12 -0800

    Modification to theme installation script to force error on failure.

--- files/scripts/install_sddm_simple_theme.sh
+++ files/scripts/install_sddm_simple_theme.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# Tell build process to exit if there are any errors.
+set -oue pipefail
+
+dnf install git
+
 git clone https://github.com/JaKooLit/simple-sddm.git ./simple_sddm
 sudo mv ./simple_sddm /usr/share/sddm/themes/
 
