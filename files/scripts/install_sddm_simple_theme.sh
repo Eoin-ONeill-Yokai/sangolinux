3bebea507b41d675973fd0729c08ed294ef5858e
Eoin O'Neill <eoinoneill1991@gmail.com>
2025-01-15 15:25:04 -0800

    Change theme to SDDM-Simple-2 for QT6 Support

--- files/scripts/install_sddm_simple_theme.sh
+++ files/scripts/install_sddm_simple_theme.sh
@@ -5,13 +5,13 @@ set -x #echo on
 
 echo "Installing Theme: simple-sddm ..."
 
-git clone https://github.com/JaKooLit/simple-sddm.git ./simple-sddm
+git clone https://github.com/JaKooLit/simple-sddm-2.git ./simple-sddm-2
 mkdir -p /usr/share/sddm/themes
-mv ./simple-sddm /usr/share/sddm/themes/
+mv ./simple-sddm-2 /usr/share/sddm/themes/
 
 cat <<EOF > /etc/sddm.conf.d/sddm.conf
 [Theme]
-Current=simple-sddm
+Current=simple-sddm-2
 EOF
 
 touch /usr/share/sddm/themes_installed.flag
