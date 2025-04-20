0a3164a06880db3abda1a162b75b6ab99ea101f4
Eoin O'Neill <eoinoneill1991@gmail.com>
2025-04-20 14:58:40 -0700

    Change nvidia to use ublue base-nvidia instead of rolling my own.

--- files/scripts/install_nvidia_toolkit_policy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# SOURCED FROM WAYBLUE: https://github.com/wayblueorg/wayblue/blob/live/files/scripts/installnvidiatoolkitpolicy.sh
-
-# Tell build process to exit if there are any errors.
-set -oue pipefail
-
-semodule --verbose --install /usr/share/selinux/packages/nvidia-container.pp
