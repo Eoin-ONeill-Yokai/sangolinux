c1de46e91390ca07b07783f24023cebfda9bd5d9
Eoin O'Neill <eoinoneill1991@gmail.com>
2025-07-25 15:00:49 -0700

    Fix theme to use name instead of number

--- files/scripts/install_kde_catppuccin.sh
+++ files/scripts/install_kde_catppuccin.sh
@@ -9,8 +9,8 @@ echo "Installing Theme: KDE Catpuccin (LATTE and MOCHA)..."
 
 git clone https://github.com/catppuccin/kde.git ./catppuccin
 cd ./catppuccin
-./install.sh Latte Teal Modern global
-./install.sh Mocha Teal Modern global
+./install.sh 4 Teal Modern global
+./install.sh 1 Teal Modern global
 
 mkdir -p /usr/share/kde
 touch /usr/share/kde/catppuccin_installed.flag
