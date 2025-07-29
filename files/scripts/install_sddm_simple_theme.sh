3a4bf0e68b64acbe72324a7e44b065808ad089a2
Eoin O'Neill <eoinoneill1991@gmail.com>
2025-07-29 12:16:44 -0700

    Fixup colors a bit on login screen

--- files/scripts/install_sddm_simple_theme.sh
+++ files/scripts/install_sddm_simple_theme.sh
@@ -14,10 +14,53 @@ cat <<EOF > /etc/sddm.conf.d/sddm.conf
 Current=simple-sddm-2
 EOF
 
-sed -i 's/^MainColor=.*$/MainColor="#b8b8b8"/g' /usr/share/sddm/themes/simple-sddm-2/theme.conf
-sed -i 's/^AccentColor=.*$/AccentColor="#01a299"/g' /usr/share/sddm/themes/simple-sddm-2/theme.conf
+# #232634 MANTLE
+# #414559 SURFACE
+# #c6d0f5 TEXT
+# #81c8be ACCENT
+# #e78284 ERROR
+# #babbf1 HIGHLIGHT BASE
+# #838ba7 HIGHLIGHT TEXT
+
+replace_color () {
+    local title=$1
+    local value=$2
+    sed -i 's/^'${title}'=.*$/${title}="'${value}'"/g' /usr/share/sddm/themes/simple-sddm-2/theme.conf
+}
+
+
+replace_color "MainColor" "#c6d0f5"
+replace_color "AccentColor" "#81c8be"
+
+replace_color "HeaderTextColor" "#c6d0f5"
+replace_color "DateTextColor" "#c6d0f5"
+replace_color "TimeTextColor" "#81c8be"
+
+replace_color "LoginFieldBackgroundColor" "#414559"
+replace_color "PasswordFieldBackgroundColor" "#414559"
+replace_color "LoginFieldTextColor" "#81c8be"
+replace_color "PasswordFieldTextColor" "#81c8be"
+replace_color "UserIconColor" "#ca9ee6"
+replace_color "PasswordIconColor" "#ca9ee6"
+
+replace_color "WarningColor" "#e78284"
+
+replace_color "LoginButtonTextColor" "#c6d0f5"
+replace_color "LoginButtonBackgroundColor" "#414559"
+replace_color "SystemButtonsIconsColor" "#c6d0f5"
+replace_color "SessionButtonTextColor" "#c6d0f5"
+replace_color "VirtualKeyboardButtonTextColor" "#9157D6"
+
+replace_color "DropdownTextColor" "#c6d0f5"
+replace_color "DropdownSelectedBackgroundColor" "#414559"
+replace_color "DropdownBackgroundColor" "#232634"
+
+replace_color "HighlightTextColor" "#232634"
+replace_color "HighlightBackgroundColor" "#81c8be"
+replace_color "HighlightBorderColor" "#414559"
+
 sed -i 's/^BlurRadius=.*$/BlurRadius="0"/g' /usr/share/sddm/themes/simple-sddm-2/theme.conf
 sed -i 's/^FormPosition=.*$/FormPosition="center"/g' /usr/share/sddm/themes/simple-sddm-2/theme.conf
-sed -i 's/^Background=.*$/Background="Backgrounds\/earth_lights_environment_globe-cc0.jpg"/g' /usr/share/sddm/themes/simple-sddm-2/theme.conf
+
 
 touch /usr/share/sddm/themes_installed.flag
