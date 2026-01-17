742b0e8509846cc8ec9ba9a86b5bc24b0ca6cc2d
Eoin O'Neill <eoinoneill1991@gmail.com>
2026-01-16 22:33:08 -0800

    Install opentabletdriver for cosmic systems

---
+++ files/scripts/install_opentabletdriver.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget https://github.com/OpenTabletDriver/OpenTabletDriver/releases/latest/download/opentabletdriver-0.6.6.2-1.x86_64.rpm
+dnf install -y ./opentabletdriver-0.6.6.2-1.x86_64.rpm
