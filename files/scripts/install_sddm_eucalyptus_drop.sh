4ea897ddd13d19b362bfcead453c5ae2c0ea4671
Eoin O'Neill <eoinoneill1991@gmail.com>
2026-08-05 00:13:37 -0700

    Fixup amate build issues (#9)

--- files/scripts/install_sddm_eucalyptus_drop.sh
+++ files/scripts/install_sddm_eucalyptus_drop.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 wget https://gitlab.com/api/v4/projects/37107648/packages/generic/sddm-eucalyptus-drop/2.0.0/sddm-eucalyptus-drop-v2.0.0.zip
 sddmthemeinstaller --install ./sddm-eucalyptus-drop-v2.0.0.zip
